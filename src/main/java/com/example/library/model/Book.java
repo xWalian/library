8e0430a656f7baa5eb4ffe9a60c4478073695898
xWalian <lukasek008@gmail.com>
2024-02-05 16:50:02 +0100

    pierwsze poprawki

--- src/main/java/com/example/library/model/Book.java
+++ src/main/java/com/example/library/model/Book.java
@@ -1,25 +1,19 @@
 package com.example.library.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Book {
 
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
     private String author;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 
     // Getters and setters
 }
