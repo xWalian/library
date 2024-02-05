bfa548f8837742498a3f7c0f5b9a088d0fec98bd
xWalian <lukasek008@gmail.com>
2024-02-05 17:21:08 +0100

    pierwsze poprawki

--- src/main/java/com/example/library/model/Book.java
+++ src/main/java/com/example/library/model/Book.java
@@ -8,12 +8,36 @@ import jakarta.persistence.Id;
 @Entity
 public class Book {
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String title;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
     public String author;
 
     // Getters and setters
+    public Long getId() {
+        return id;
+    }
 }
